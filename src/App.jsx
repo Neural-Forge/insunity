557e4ad04e302ac3cd7f0dd10662f9cbac56eded
Enock <enock.mokua@student.moringaschool.com>
2023-04-10 22:27:16 +0300

    set up react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">
-
-  </div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
