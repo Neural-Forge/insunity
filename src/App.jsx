070a719dca5edaa5749fdfea5bcc96fcf5842f99
Enock <enock.mokua@student.moringaschool.com>
2023-04-10 22:58:01 +0300

    remove styles for direct styles from parent compoennt

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./Pages/landing/landing";
 import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
